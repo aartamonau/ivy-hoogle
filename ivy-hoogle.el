ae76af638d2c0d5089ee0f825b330562376d80a2
Aliaksei Artamonau <aliaksiej.artamonau@gmail.com>
2023-05-08 23:49:15 -0700

    grouping

--- ivy-hoogle.el
+++ ivy-hoogle.el
@@ -1,11 +1,12 @@
 (require 'async)
+(require 'cl-lib)
 (require 'haskell-font-lock)
 (require 'seq)
 
 (defgroup ivy-hoogle-appearance nil
   "Ivy Hoogle Appearance.")
 
-(defcustom ivy-hoogle-delay-ms 100
+(defcustom ivy-hoogle-delay-ms 200
   "TODO"
   :type 'integer)
 
@@ -26,16 +27,18 @@
 (ivy-configure 'ivy-hoogle
   :format-fn #'ivy-hoogle--format-function)
 
+(cl-defstruct ivy-hoogle-source
+  url
+  package
+  package-url
+  module
+  module-url)
+
 (cl-defstruct ivy-hoogle-candidate
   "TODO"
   item
-  url
-  module
-  module-url
-  package
-  package-url
+  sources
   doc-html
-  doc-text
 
   formatted
   width)
@@ -45,6 +48,81 @@
 (defvar ivy-hoogle--cache (make-hash-table :test 'equal))
 (defvar ivy-hoogle--process-query nil)
 (defvar ivy-hoogle--process nil)
+
+(defun ivy-hoogle--group-by (elems key-fn)
+  (let ((groups (make-hash-table :test #'equal))
+        (keys (mapcar key-fn elems)))
+    (cl-loop for (key . elem) in (cl-mapcar #'cons keys elems)
+             do
+             (let ((key-elems (gethash key groups)))
+               (puthash key (cons elem key-elems) groups)))
+    (cl-loop for key in keys with result = nil
+             do
+             (let ((group (nreverse (gethash key groups))))
+               (when group
+                 (remhash key groups)
+                 (push (cons key group) result)))
+             finally return (nreverse result))))
+
+(defun ivy-hoogle--format-sources (sources)
+  (let ((sources-by-package (ivy-hoogle--group-by sources #'ivy-hoogle-source-package)))
+    (cl-loop for (package . package-sources) in sources-by-package
+             when (not (null package))
+             collect (let ((modules (mapcar #'ivy-hoogle-source-module package-sources)))
+                       (setq modules (seq-remove #'null modules))
+                       (string-join (cons package modules) " "))
+             into result
+             finally return (string-join result ", "))))
+
+(defun ivy-hoogle--source-lessp (source-a source-b)
+  (let ((package-a (ivy-hoogle-source-package source-a))
+        (package-b (ivy-hoogle-source-package source-b))
+        (module-a (ivy-hoogle-source-module source-a))
+        (module-b (ivy-hoogle-source-module source-b)))
+    (if (string-equal package-a package-b)
+        (string-lessp module-a module-b)
+      (string-lessp package-a package-b))))
+
+(defun ivy-hoogle--merge-candidates (candidates)
+  (let ((candidate (car candidates))
+        (sources (apply #'append (mapcar #'ivy-hoogle-candidate-sources candidates))))
+    (setq sources (sort sources #'ivy-hoogle--source-lessp))
+    (setf (ivy-hoogle-candidate-sources candidate) sources)
+    candidate))
+
+(ivy-hoogle--format-candidates
+ (list
+  (ivy-hoogle--merge-candidates
+   (list (make-ivy-hoogle-candidate
+          :item "foldl :: Foldable t => (b -> a -> b) -> b -> t a -> b"
+          :doc-html "foldl"
+          :sources (list (make-ivy-hoogle-source :url "url1" :package "base" :module "Prelude")))
+         (make-ivy-hoogle-candidate
+          :item "foldl :: Foldable t => (b -> a -> b) -> b -> t a -> b"
+          :doc-html "foldl"
+          :sources (list (make-ivy-hoogle-source :url "url2" :package "base" :module "Data.List")))))))
+
+(defun ivy-hoogle--group-candidates (candidates)
+  (let* ((key-fn (lambda (candidate)
+                   (cons (ivy-hoogle-candidate-item candidate)
+                         (ivy-hoogle-candidate-doc-html candidate))))
+         (groups (ivy-hoogle--group-by candidates key-fn)))
+    (cl-loop for (_ . group) in groups
+             collect (ivy-hoogle--merge-candidates group))))
+
+(ivy-hoogle--group-candidates
+ (list (make-ivy-hoogle-candidate
+        :item "foldl :: Foldable t => (b -> a -> b) -> b -> t a -> b"
+        :doc-html "foldl"
+        :sources (list (make-ivy-hoogle-source :url "url1" :package "base" :module "Prelude")))
+       (make-ivy-hoogle-candidate
+        :item "foldl :: Foldable t => (b -> a -> b) -> b -> t a -> b"
+        :doc-html "foldl"
+        :sources (list (make-ivy-hoogle-source :url "url2" :package "base" :module "Data.List")))
+       (make-ivy-hoogle-candidate
+        :item "foldl :: (b -> a -> b) -> b -> [a] -> b"
+        :doc-html "foldl for lists"
+        :sources (list (make-ivy-hoogle-source :url "url3" :package "xmonad" :module "XMonad.Core")))))
 
 (defun ivy-hoogle--set-candidates (candidates)
   ;; TODO: how do I avoid using an internal variable
@@ -61,21 +139,12 @@
 
 (defun ivy-hoogle--format-candidate (width candidate)
   (let* ((item (ivy-hoogle-candidate-item candidate))
-         (package (ivy-hoogle-candidate-package candidate))
-         (module (ivy-hoogle-candidate-module candidate))
-         (package-and-module
-          (let ((joined (string-join
-                         (seq-remove 'null (list package module))
-                         " ")))
-            (if joined
-                (concat "(" joined ")")
-              "")))
+         (sources (ivy-hoogle--format-sources (ivy-hoogle-candidate-sources candidate)))
          (min-spaces 4)
          (width-remaining (- width (length item))))
-    (apply 'concat
+    (apply #'concat
            (haskell-fontify-as-mode item 'haskell-mode)
-           (let* ((short (ivy-hoogle--shorten package-and-module
-                                              (- width-remaining min-spaces)))
+           (let* ((short (ivy-hoogle--shorten sources (- width-remaining min-spaces)))
                   (num-spaces (- width-remaining (length short))))
              (unless (string-empty-p short)
                (list (make-string num-spaces ?\ )
@@ -84,12 +153,13 @@
 (defun ivy-hoogle--format-candidates (candidates)
   (let ((width (window-width)))
     (mapcar (lambda (candidate)
-              (if (equal (ivy-hoogle-candidate-width candidate) width)
-                  (copy-sequence (ivy-hoogle-candidate-formatted candidate))
-                (let ((formatted (ivy-hoogle--format-candidate width candidate)))
-                  (setf (ivy-hoogle-candidate-formatted candidate) formatted
-                        (ivy-hoogle-candidate-width candidate) width)
-                  (copy-sequence formatted))))
+              (cond ((stringp candidate) candidate)
+                    ((equal (ivy-hoogle-candidate-width candidate) width)
+                     (copy-sequence (ivy-hoogle-candidate-formatted candidate)))
+                    (t (let ((formatted (ivy-hoogle--format-candidate width candidate)))
+                         (setf (ivy-hoogle-candidate-formatted candidate) formatted
+                               (ivy-hoogle-candidate-width candidate) width)
+                         (copy-sequence formatted)))))
             candidates)))
 
 (defun ivy-hoogle--format-function (candidates)
@@ -112,24 +182,28 @@
          (package-url (gethash "url" package-obj))
          (doc-html (gethash "docs" parsed)))
     (when (and item url)
+      (let ((source (make-ivy-hoogle-source
+                     :url url
+                     :module module
+                     :module-url module-url
+                     :package package
+                     :package-url package-url)))
       (make-ivy-hoogle-candidate
        :item item
-       :url url
        :doc-html doc-html
-       :module module
-       :module-url module-url
-       :package package
-       :package-url package-url))))
+       :sources (list source))))))
 
 (defun ivy-hoogle--on-finish (process)
   (let* ((output (with-current-buffer (process-buffer process) (buffer-string)))
          (lines (string-lines output t))
-         (candidates (pcase lines
-                       ('("No results found") nil)
-                       (_ (cl-loop for line in lines
-                                   unless (string-prefix-p "--" line)
-                                   for item = (ivy-hoogle--parse-item line)
-                                   collect item)))))
+         candidates)
+    (cl-loop for line in lines
+             unless (or (string-prefix-p "--" line)
+                        (string-prefix-p "No results found" line))
+             collect (ivy-hoogle--parse-item line) into results
+             finally (setq candidates (ivy-hoogle--group-candidates results)))
+    (unless candidates
+      (setq candidates (list "No results found")))
     (puthash ivy-hoogle--process-query candidates ivy-hoogle--cache)
     (ivy-hoogle--set-candidates candidates)
     (ivy-update-candidates candidates)
@@ -145,20 +219,6 @@
     (setq ivy-hoogle--process-query query)
     (setq ivy-hoogle--process
           (apply 'async-start-process "hoogle" "hoogle" 'ivy-hoogle--on-finish args))))
-
-;; TODO: handle errors
-(defun ivy-hoogle--run-hoogle (query)
-  (let* ((args `("search"
-                 "--count" ,(number-to-string ivy-hoogle-num-candidates)
-                 "--jsonl"
-                 ,query))
-         (lines (apply 'process-lines "hoogle" args)))
-    (pcase lines
-      ('("No results found") nil)
-      (_ (cl-loop for line in lines
-                  unless (string-prefix-p "--" line)
-                  for item = (ivy-hoogle--parse-item line)
-                  collect item)))))
 
 (defun ivy-hoogle--queue-update (query)
   (ivy-hoogle--cleanup-timer)
@@ -210,6 +270,7 @@
      'ivy-hoogle--candidates
      :action 'ivy-hoogle--action
      :dynamic-collection t
+     :require-match t
      :unwind 'ivy-hoogle--cleanup
      :history 'ivy-hoogle--history
      :caller 'ivy-hoogle)))
