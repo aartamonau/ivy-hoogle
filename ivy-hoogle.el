a888f2cc28864a28ce995e5218c05569ce079b2d
Aliaksei Artamonau <aliaksiej.artamonau@gmail.com>
2023-08-20 01:46:38 -0700

    License

--- ivy-hoogle.el
+++ ivy-hoogle.el
@@ -8,6 +8,21 @@
 ;; Package-Requires: ((emacs "27.2"))
 ;; Keywords: matching haskell hoogle
 
+;; This file is part of ivy-hoogle.
+
+;; ivy-hoogle is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; ivy-hoogle is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with ivy-hoogle.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;;; Code:
