9aa38e3faec476d6f1e65f11a765a0e042b5b94f
Aliaksei Artamonau <aliaksiej.artamonau@gmail.com>
2023-09-02 14:03:21 -0700

    Add a short comment.

--- ivy-hoogle.el
+++ ivy-hoogle.el
@@ -25,6 +25,12 @@
 
 ;;; Commentary:
 
+;; This package provides `ivy-hoogle' and `ivy-hoogle-thing-at-point' that allow
+;; querying Hoogle, the Haskell API search engine, using `ivy-read'-based
+;; interface.
+;;
+;; See README.md for further details.
+
 ;;; Code:
 
 (require 'async)
